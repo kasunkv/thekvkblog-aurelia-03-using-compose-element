4768b26a2a9f196f0cfbc124c925146bebdb4289
kasunkv <kvkrusl@gmail.com>
2016-09-18 20:16:14 +0530

    Completed the demo

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import {LogManager} from 'aurelia-framework';
 import {ConsoleAppender} from 'aurelia-logging-console';
 
 LogManager.addAppender(new ConsoleAppender());
-LogManager.setLevel(LogManager.logLevel.info);
+LogManager.setLevel(LogManager.logLevel.debug);
 
 export function configure(aurelia) {
     aurelia.use.standardConfiguration();
-    aurelia.start().then(a => a.setRoot('home', document.getElementById('app-container')));
+    aurelia.start().then(a => a.setRoot('shell', document.getElementById('app-container')));
 }
